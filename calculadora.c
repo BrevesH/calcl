2df8e9fdbdb0936adc3b11fe9cb90c886682e7d8
BrevesH <brevesakgodz@gmail.com>
2025-05-22 17:13:56 -0300

    mudei

--- calculadora.c
+++ calculadora.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
-
+#include <math.h>
 void main ()
-
 {
-float num1, num2, resultado, resultado2;
-     printf("digite um valor");
-     scanf("%f", &num1);
 
-     printf("digite um valor");
-     scanf("%f", &num2);
+    float valor1, valor2;
+    float resultadoSoma, resultadoSub, resultadoMult, resultadoDiv;
+
+
+
+    printf("Digite o primeiro valor: ");
+    scanf("%f", &valor1);
+    printf("Digite o segundo valor: ");
+    scanf("%f", &valor2);
+    
+    resultadoSoma = valor1 + valor2;
+    resultadoSub = valor1 - valor2;
+    resultadoMult = valor1 * valor2;
+    resultadoDiv = valor1 / valor2;
+    if (valor2 == 0) {
+        printf("Erro: Divisão por zero não é permitida.\n");
+        return;
+    
 
     
-     resultado = num1 * num2;
-     resultado2 = num1/num2;
+
     
 
-     printf("%f", resultado);
-     printf("%f", resultado2);
+
+    printf("A soma de %f e %f é: %f\n", valor1, valor2, resultadoSoma);
+    printf("A subtração de %f e %f é: %f\n", valor1, valor2, resultadoSub);
+    printf("A multiplicação de %f e %f é: %f\n", valor1, valor2, resultadoMult);   
+    printf("A divisão de %f e %f é: %f\n", valor1, valor2, resultadoDiv);  
+
+
+    printf("A raiz quadrada de %d é: %.2f\n", valor1, sqrt(valor1));
+    printf("A raiz quadrada de %d é: %.2f\n", valor2, sqrt(valor2));
 
 
 }
+
+
+
 
